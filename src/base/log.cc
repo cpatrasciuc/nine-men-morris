944f2776c582a42a0fa4c9aba11098a353131359
Cristian Patrasciuc <cristian.patrasciuc@gmail.com>
2012-09-16 17:47:08 +0300

    Added basic logging functionality.

---
+++ src/base/log.cc
@@ -0,0 +1,68 @@
+// Copyright (c) 2012 Cristian Patrasciuc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/log.h"
+
+#include <time.h>
+#include <iostream>
+#include <sstream>
+#include <string>
+
+namespace base {
+
+LogLevel Log::max_log_level = DEBUG;
+
+std::ostream& Log::default_output_stream = std::cerr;
+
+inline std::string LogLevelToString(LogLevel log_level) {
+  switch (log_level) {
+    case ERROR:
+      return "ERROR";
+    case WARNING:
+      return "WARNING";
+    case INFO:
+      return "INFO";
+    case DEBUG:
+      return "DEBUG";
+  }
+}
+
+std::string GetCurrentTime() {
+  time_t rawtime;
+  time(&rawtime);
+  const size_t kMaxBufferSize = 20;
+  char buffer[kMaxBufferSize];
+  tm time_info;
+  localtime_r(&rawtime, &time_info);
+  size_t len = strftime(buffer, kMaxBufferSize, "%b %d %X", &time_info);
+  if (len) {
+    return std::string(buffer, len);
+  }
+  return std::string();
+}
+
+LogMessage::LogMessage(LogLevel log_level,
+                       int line_number,
+                       const char* file_name,
+                       std::ostream& stream)
+    : stream_(stream) {
+  PrintHeader(log_level, line_number, file_name);
+}
+
+LogMessage::~LogMessage() {
+  stream_ << std::endl;
+}
+
+void LogMessage::PrintHeader(LogLevel log_level, int line_number,
+                             const char* file_name) {
+  stream_ << "[";
+  stream_ << LogLevelToString(log_level);
+  stream_ << "][";
+  stream_ << GetCurrentTime();
+  stream_ << "][";
+  stream_ << getpid();
+  // TODO(cpatrasciuc): Add thread ID
+  stream_ << "][" << file_name << "(" << line_number << ")] ";
+}
+}
---
+++ src/base/log.h
@@ -0,0 +1,80 @@
+// Copyright (c) 2012 Cristian Patrasciuc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_LOG_H_
+#define BASE_LOG_H_
+
+#include <iostream>
+#include <ostream>
+
+// This file provides the logging functionality through a set of macros.
+// You can use constructs like the following throughout the source code
+// to generate logging statements.
+// Example:
+//
+//   LOG(INFO) << "This is some useful info";
+//   LOG_IF(ERROR, p == NULL) << "The pointer is NULL";
+//
+// The output for these examples would be something similar to:
+//
+//   [INFO][Sep 16 16:16:27][9513][source_file.cc(10)] This is some useful info
+//   [ERROR][Sep 16 16:16:27][9513][source_file.cc(11)] The pointer is NULL
+//
+// It is also possible to call a succession of insertions operations as:
+//
+//    LOG(INFO) << "The value of foo is " << foo << ".";
+//
+// If ENABLE_LOGGING is not defined, all log statements are optimized out at
+// compile time.
+
+namespace base {
+
+enum LogLevel {
+  ERROR,
+  WARNING,
+  INFO,
+  DEBUG
+};
+
+// Contains global logging settings
+class Log {
+ public:
+  // Every logging statement with a level greater than this, is discarded
+  static LogLevel max_log_level;
+
+  // The default output stream to which LogMessage's are sent,
+  // if no specific stream is specified at instantiation.
+  static std::ostream& default_output_stream;
+};
+
+// Represents
+class LogMessage {
+ public:
+  LogMessage(LogLevel log_level,
+          int line_number,
+          const char* file_name,
+          std::ostream& stream = Log::default_output_stream);
+  ~LogMessage();
+
+  std::ostream& stream() const {
+    return stream_;
+  }
+
+ private:
+  void PrintHeader(LogLevel log_level, int line_number, const char* file_name);
+  std::ostream& stream_;
+};
+
+#ifdef ENABLE_LOGGING
+#define LOG_IF(level, cond) if ((level > Log::max_log_level) || !(cond)) ; \
+  else LogMessage(level, __LINE__, __FILE__).stream()
+#else
+#define LOG_IF(level, cond) if (false) std::cerr
+#endif
+
+#define LOG(level) LOG_IF(level, true)
+
+}  // namespace
+
+#endif  // BASE_LOG_H_
