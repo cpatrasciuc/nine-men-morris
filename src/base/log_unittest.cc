9519ae20a880b03a596f8bbf5ebdf038f4036cd7
Cristian Patrasciuc <cristian.patrasciuc@gmail.com>
2012-09-17 00:35:17 +0300

    Extract the common test_stream into a LogUnittest field.

--- src/base/log_unittest.cc
+++ src/base/log_unittest.cc
@@ -29,6 +29,8 @@ class LogUnittest : public ::testing::Test {
   virtual void SetUp() {
     old_stream_ = Log::default_output_stream;
     old_log_level_ = Log::max_log_level;
+    Log::default_output_stream = &test_stream_;
+    Log::max_log_level = DEBUG;
   }
 
   virtual void TearDown() {
@@ -36,7 +38,12 @@ class LogUnittest : public ::testing::Test {
     Log::max_log_level = old_log_level_;
   }
 
+  std::ostringstream& test_stream() {
+    return test_stream_;
+  }
+
  private:
+  std::ostringstream test_stream_;
   std::ostream* old_stream_;
   LogLevel old_log_level_;
 };
@@ -44,14 +51,13 @@ class LogUnittest : public ::testing::Test {
 TEST_F(LogUnittest, MAYBE(LogLevelFilteringTest)) {
   LogLevel levels[] = {ERROR, WARNING, INFO, DEBUG};
   for (unsigned int level = 0; level < arraysize(levels); ++level) {
-    std::ostringstream test_stream;
-    Log::default_output_stream = &test_stream;
+    test_stream().str("");  // Clear the buffer
     Log::max_log_level = levels[level];
     LOG(ERROR)   << "Error message";
     LOG(WARNING) << "Warning message";
     LOG(INFO)    << "Info message";
     LOG(DEBUG)   << "Debug message";
-    std::string output = test_stream.str();
+    std::string output = test_stream().str();
     const unsigned int lines_count =
         std::count(output.begin(), output.end(), '\n');
     EXPECT_EQ(level + 1, lines_count);
@@ -59,25 +65,21 @@ TEST_F(LogUnittest, MAYBE(LogLevelFilteringTest)) {
 }
 
 TEST_F(LogUnittest, MAYBE(LogIfTest)) {
-  std::ostringstream test_stream;
-  Log::default_output_stream = &test_stream;
-  Log::max_log_level = DEBUG;
   LOG_IF(INFO, 0 == 1) << "Should not be logged";
-  EXPECT_EQ(std::string(), test_stream.str());
+  EXPECT_EQ(std::string(), test_stream().str());
   std::string message("Should be logged");
   LOG_IF(INFO, 0 != 1) << message;
-  EXPECT_NE(std::string::npos, test_stream.str().find(message));
+  EXPECT_NE(std::string::npos, test_stream().str().find(message));
 }
 
 TEST_F(LogUnittest, LogMessageFormatTest) {
-  std::ostringstream test_stream;
   std::string source_file("foobar.cc");
   std::string text_message("Foo Bar");
   {
-    LogMessage log_message(INFO, 69, source_file.c_str(), test_stream);
+    LogMessage log_message(INFO, 69, source_file.c_str(), test_stream());
     log_message.stream() << text_message;
   }
-  std::string output = test_stream.str();
+  std::string output = test_stream().str();
   EXPECT_NE(std::string::npos, output.find("INFO"));
   EXPECT_NE(std::string::npos, output.find("69"));
   EXPECT_NE(std::string::npos, output.find(source_file));
@@ -85,20 +87,18 @@ TEST_F(LogUnittest, LogMessageFormatTest) {
 }
 
 TEST_F(LogUnittest, MAYBE(SystemErrorLogIfTest)) {
-  std::ostringstream test_stream;
-  Log::default_output_stream = &test_stream;
-  Log::max_log_level = DEBUG;
   float result = std::pow(10.0f, 2.0f);
   ELOG_IF(ERROR, result == HUGE_VAL) << "Should not be logged";
-  EXPECT_EQ(std::string(), test_stream.str());
+  EXPECT_EQ(std::string(), test_stream().str());
 
   std::string message("Should be logged");
   float max_float = std::numeric_limits<float>::max();
   result = std::pow(max_float, max_float);
   ELOG_IF(ERROR, result == HUGE_VAL) << message;
-  size_t pos = test_stream.str().find(message);
+  std::string output = test_stream().str();
+  size_t pos = output.find(message);
   EXPECT_NE(std::string::npos, pos);
-  EXPECT_LT(pos, test_stream.str().size() - message.size());
+  EXPECT_LT(pos, output.size() - message.size());
 }
 
 }  // anonymous namespace
