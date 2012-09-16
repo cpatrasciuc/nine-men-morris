755fd018c07dcad10596364f41a8bd7f5b97fa3b
Cristian Patrasciuc <cristian.patrasciuc@gmail.com>
2012-09-17 01:40:32 +0300

    Added the pre-commit hook script. It that runs cpplint on .cc and .h file. Use:  ln -s <git-root>/tools/pre-commit-hok.py <git-root>/.git/hook/pre-commit

---
+++ tools/pre-commit-hook.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python
+# Quick script that runs cpplint on all .cc or .h files before 'git commit'
+
+import os
+import sys
+
+git_dir = os.popen("git rev-parse --show-cdup").readlines()[0][:-1]
+cpplint_path = os.path.join(git_dir, "tools/cpplint.py")
+
+files = os.popen("git diff --cached --name-only").readlines()
+files = [os.path.join(git_dir, x[:-1]) for x in files]
+
+cmd = "python '%s' --filter=-readability/streams %s"
+ret = os.system(cmd % (cpplint_path, " ".join(files)))
+if ret != 0:
+	print "Exit code: %s" % ret
+	sys.exit(1)
