02112ffb6a65b970e0fc52672d44b76f944a4ea8
Cristian Patrasciuc <cristian.patrasciuc@gmail.com>
2012-09-20 15:54:02 +0300

    Enable stack trace dump on test crashes

--- src/base/test_runner.cc
+++ src/base/test_runner.cc
@@ -4,9 +4,11 @@
 
 // TODO(cpatrasciuc): Auto-generated this file when a test target is built.
 
+#include "base/debug/stacktrace.h"
 #include "gtest/gtest.h"
 
 int main(int argc, char **argv) {
+  ::base::debug::EnableStackTraceDumpOnCrash();
   ::testing::InitGoogleTest(&argc, argv);
   return RUN_ALL_TESTS();
 }
