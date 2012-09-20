2951e6bfe454508f7eed0c5a69bc20fad4bd0563
Cristian Patrasciuc (Home) <cristian.patrasciuc@gmail.com>
2012-09-20 23:08:37 +0300

    Add the SHOULD_NOT_COMPILE macro

--- src/base/basic_macros.h
+++ src/base/basic_macros.h
@@ -15,5 +15,12 @@
 
 // TODO(macros): Add a macro from __attribute__(override)
 
+// This macro should be used in tests for specifying that specific statements
+// should not compile. When the unit under test is modified it's easy to quickly
+// check that the compile-time restrictions are still valid by temporarily 
+// removing the macros. It also helps to understand better the expected
+// behavior of the class that is tested.
+#define SHOULD_NOT_COMPILE(statement)
+
 #endif  // BASE_BASIC_MACROS_H_
 
--- src/base/ptr/smart_ptr_unittest.cc
+++ src/base/ptr/smart_ptr_unittest.cc
@@ -2,6 +2,7 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include "base/basic_macros.h"
 #include "base/ptr/default_storage_policy.h"
 #include "base/ptr/smart_ptr.h"
 #include "gtest/gtest.h"
@@ -44,17 +45,17 @@ TEST(SmartPtrTest, Const) {
   const int* x = NULL;
   SmartPtr<const int> ptr1(x);
   // TODO(macros): Add a macro to specify that something should not compile
-  // *ptr1 = 10;  // Should not compile
+  SHOULD_NOT_COMPILE(*ptr1 = 10);
   Get(ptr1);
 
   SmartPtr<const Helper> ptr2(new Helper);
   ptr2->GetPrivateMember();
-  // ptr2->SetPrivateMember(10);  // Should not compile
+  SHOULD_NOT_COMPILE(ptr2->SetPrivateMember(10));
 
   const SmartPtr<const Helper> ptr3(new Helper);
   ptr3->GetPrivateMember();
-  // ptr3->SetPrivateMember(10);  // Should not compile
-  // ptr3 = ptr2;  // Should not compile
+  SHOULD_NOT_COMPILE(ptr3->SetPrivateMember(10));
+  SHOULD_NOT_COMPILE(ptr3 = ptr2);
   // TODO(smart_pointer): ptr2 = ptr3;
 }
 
