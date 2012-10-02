6bf1c76bc30cdfde0bc8813a6f65f907233eb7f5
Cristian Patrasciuc <cristian.patrasciuc@gmail.com>
2012-10-02 15:58:54 +0300

    Added support for methods as callables

--- src/base/bind_unittest.cc
+++ src/base/bind_unittest.cc
@@ -2,65 +2,139 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <string>
+
 #include "base/callable.h"
 #include "base/function.h"
+#include "base/method.h"
+#include "base/ptr/scoped_ptr.h"
 #include "gtest/gtest.h"
 
 namespace base {
+namespace {
 
-int f ()                           { return 69; }
+int f ()                           { return 10; }
 int f1(int x)                      { return x; }
 int f2(int x, int y)               { return x * y; }
 int f3(int x, int y, int z)        { return x * y * z; }
 int f4(int x, int y, int z, int t) { return x * y * z * t; }
 
-TEST(FunctionTest, Basic) {
-  Function<int(void)> func(&f);
-  Function<int(int)> func1(&f1);
-  Function<int(int, int)> func2(&f2);
-  Function<int(int, int, int)> func3(&f3);
-  Function<int(int, int, int, int)> func4(&f4);
-
-  Callable<int(void)>& c = func;
-  Callable<int(int)>& c1 = func1;
-  Callable<int(int, int)>& c2 = func2;
-  Callable<int(int, int, int)>& c3 = func3;
-  Callable<int(int, int, int, int)>& c4 = func4;
-
-  EXPECT_EQ(69, c());
-  EXPECT_EQ(23, c1(23));
-  EXPECT_EQ(10, c2(2, 5));
-  EXPECT_EQ(12, c3(2, 3, 2));
-  EXPECT_EQ(24, c4(1, 2, 3, 4));
-}
-
 class StaticHelper {
  public:
-  static int f ()                           { return 69; }
+  static int f ()                           { return 10; }
   static int f1(int x)                      { return x; }
   static int f2(int x, int y)               { return x * y; }
   static int f3(int x, int y, int z)        { return x * y * z; }
   static int f4(int x, int y, int z, int t) { return x * y * z * t; }
 };
 
-TEST(FunctionTest, StaticMethods) {
-  Function<int(void)> func(&StaticHelper::f);
-  Function<int(int)> func1(&StaticHelper::f1);
-  Function<int(int, int)> func2(&StaticHelper::f2);
-  Function<int(int, int, int)> func3(&StaticHelper::f3);
-  Function<int(int, int, int, int)> func4(&StaticHelper::f4);
+class Helper {
+ public:
+  explicit Helper(int x) : x_(x) {}
 
-  Callable<int(void)>& c = func;
-  Callable<int(int)>& c1 = func1;
-  Callable<int(int, int)>& c2 = func2;
-  Callable<int(int, int, int)>& c3 = func3;
-  Callable<int(int, int, int, int)>& c4 = func4;
+  int f ()                           { return 10 * x_; }
+  int f1(int x)                      { return x * x_; }
+  int f2(int x, int y)               { return x * y * x_; }
+  int f3(int x, int y, int z)        { return x * y * z * x_; }
+  int f4(int x, int y, int z, int t) { return x * y * z * t * x_; }
 
-  EXPECT_EQ(69, c());
+ private:
+  int x_;
+};
+
+class ConstHelper {
+ public:
+  explicit ConstHelper(int x) : x_(x) {}
+
+  int f ()                           const { return 10 * x_; }
+  int f1(int x)                      const { return x * x_; }
+  int f2(int x, int y)               const { return x * y * x_; }
+  int f3(int x, int y, int z)        const { return x * y * z * x_; }
+  int f4(int x, int y, int z, int t) const { return x * y * z * t * x_; }
+
+ private:
+  int x_;
+};
+
+void GenericCallableTest(Callable<int(void)>& c,
+                         Callable<int(int)>& c1,
+                         Callable<int(int, int)>& c2,
+                         Callable<int(int, int, int)>& c3,
+                         Callable<int(int, int, int, int)>& c4) {
+  EXPECT_EQ(10, c());
   EXPECT_EQ(23, c1(23));
   EXPECT_EQ(10, c2(2, 5));
   EXPECT_EQ(12, c3(2, 3, 2));
   EXPECT_EQ(24, c4(1, 2, 3, 4));
 }
 
+TEST(CallableTest, Functions) {
+  Function<int(void)> func(&f);
+  Function<int(int)> func1(&f1);
+  Function<int(int, int)> func2(&f2);
+  Function<int(int, int, int)> func3(&f3);
+  Function<int(int, int, int, int)> func4(&f4);
+
+  GenericCallableTest(func, func1, func2, func3, func4);
+}
+
+TEST(CallableTest, StaticMethods) {
+  Function<int(void)> func(&StaticHelper::f);
+  Function<int(int)> func1(&StaticHelper::f1);
+  Function<int(int, int)> func2(&StaticHelper::f2);
+  Function<int(int, int, int)> func3(&StaticHelper::f3);
+  Function<int(int, int, int, int)> func4(&StaticHelper::f4);
+
+  GenericCallableTest(func, func1, func2, func3, func4);
+}
+
+TEST(CallableTest, Methods) {
+  Helper h(1);
+
+  Method<int(Helper::*)(void)> m(&Helper::f, &h);
+  Method<int(Helper::*)(int)> m1(&Helper::f1, &h);
+  Method<int(Helper::*)(int, int)> m2(&Helper::f2, &h);
+  Method<int(Helper::*)(int, int, int)> m3(&Helper::f3, &h);
+  Method<int(Helper::*)(int, int, int, int)> m4(&Helper::f4, &h);
+
+  GenericCallableTest(m, m1, m2, m3, m4);
+}
+
+TEST(CallableTest, ConstMethods) {
+  ConstHelper h(1);
+
+  Method<int(ConstHelper::*)(void) const> m(&ConstHelper::f, &h);
+  Method<int(ConstHelper::*)(int) const> m1(&ConstHelper::f1, &h);
+  Method<int(ConstHelper::*)(int, int) const> m2(&ConstHelper::f2, &h);
+  Method<int(ConstHelper::*)(int, int, int) const> m3(&ConstHelper::f3, &h);
+  Method<int(ConstHelper::*)(int, int, int, int) const> m4(
+      &ConstHelper::f4, &h);
+
+  GenericCallableTest(m, m1, m2, m3, m4);
+}
+
+class Base {
+ public:
+  virtual ~Base() {}
+  virtual std::string Foo(const char* s) {
+    return "This should not be called";
+  }
+};
+
+class Derived : public Base {
+ public:
+  virtual std::string Foo(const char* s) {
+    return std::string(s);
+  }
+};
+
+TEST(CallableTest, VirtualMethods) {
+  using base::ptr::scoped_ptr;
+  scoped_ptr<Base> b(new Derived);
+  Method<std::string(Base::*)(const char*)> m(&Base::Foo, Get(b));
+  std::string test("test");
+  EXPECT_EQ(test, m(test.c_str()));
+}
+
+}  // anonymous namespace
 }  // namespace base
---
+++ src/base/method.h
@@ -0,0 +1,161 @@
+// Copyright (c) 2012 Cristian Patrasciuc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef BASE_METHOD_H_
+#define BASE_METHOD_H_
+
+#include "base/callable.h"
+
+namespace base {
+
+template <typename Signature>
+class Method : public Callable<Signature> {};
+
+template <class T, class R>
+class Method<R(T::*)(void)> : public Callable<R(void)> {
+ public:
+  Method(R (T::*f)(void), T* t) : f_(f), t_(t) {}
+
+  virtual R operator()(void) const {
+    return (t_->*f_)();
+  }
+
+ private:
+  R (T::*f_)(void);
+  T* t_;
+};
+
+template <class T, class R, class A1>
+class Method<R(T::*)(A1)> : public Callable<R(A1)> {
+ public:
+  Method(R (T::*f)(A1), T* t) : f_(f), t_(t) {}
+
+  virtual R operator()(A1 a1) const {
+    return (t_->*f_)(a1);
+  }
+
+ private:
+  R (T::*f_)(A1);
+  T* t_;
+};
+
+template <class T, class R, class A1, class A2>
+class Method<R(T::*)(A1, A2)> : public Callable<R(A1, A2)> {
+ public:
+  Method(R (T::*f)(A1, A2), T* t) : f_(f), t_(t) {}
+
+  virtual R operator()(A1 a1, A1 a2) const {
+    return (t_->*f_)(a1, a2);
+  }
+
+ private:
+  R (T::*f_)(A1, A2);
+  T* t_;
+};
+
+template <class T, class R, class A1, class A2, class A3>
+class Method<R(T::*)(A1, A2, A3)> : public Callable<R(A1, A2, A3)> {
+ public:
+  Method(R (T::*f)(A1, A2, A3), T* t) : f_(f), t_(t) {}
+
+  virtual R operator()(A1 a1, A2 a2, A3 a3) const {
+    return (t_->*f_)(a1, a2, a3);
+  }
+
+ private:
+  R (T::*f_)(A1, A2, A3);
+  T* t_;
+};
+
+template <class T, class R, class A1, class A2, class A3, class A4>
+class Method<R(T::*)(A1, A2, A3, A4)> : public Callable<R(A1, A2, A3, A4)> {
+ public:
+  Method(R (T::*f)(A1, A2, A3, A4), T* t) : f_(f), t_(t) {}
+
+  virtual R operator()(A1 a1, A2 a2, A3 a3, A4 a4) const {
+    return (t_->*f_)(a1, a2, a3, a4);
+  }
+
+ private:
+  R (T::*f_)(A1, A2, A3, A4);
+  T* t_;
+};
+
+// Const Methods
+
+template <class T, class R>
+class Method<R(T::*)(void) const> : public Callable<R(void)> {
+ public:
+  Method(R (T::*f)(void) const, T* t) : f_(f), t_(t) {}
+
+  virtual R operator()(void) const {
+    return (t_->*f_)();
+  }
+
+ private:
+  R (T::*f_)(void) const;
+  T* t_;
+};
+
+template <class T, class R, class A1>
+class Method<R(T::*)(A1) const> : public Callable<R(A1)> {
+ public:
+  Method(R (T::*f)(A1) const, T* t) : f_(f), t_(t) {}
+
+  virtual R operator()(A1 a1) const {
+    return (t_->*f_)(a1);
+  }
+
+ private:
+  R (T::*f_)(A1) const;
+  T* t_;
+};
+
+template <class T, class R, class A1, class A2>
+class Method<R(T::*)(A1, A2) const> : public Callable<R(A1, A2)> {
+ public:
+  Method(R (T::*f)(A1, A2) const, T* t) : f_(f), t_(t) {}
+
+  virtual R operator()(A1 a1, A1 a2) const {
+    return (t_->*f_)(a1, a2);
+  }
+
+ private:
+  R (T::*f_)(A1, A2) const;
+  T* t_;
+};
+
+template <class T, class R, class A1, class A2, class A3>
+class Method<R(T::*)(A1, A2, A3) const> : public Callable<R(A1, A2, A3)> {
+ public:
+  Method(R (T::*f)(A1, A2, A3) const, T* t) : f_(f), t_(t) {}
+
+  virtual R operator()(A1 a1, A2 a2, A3 a3) const {
+    return (t_->*f_)(a1, a2, a3);
+  }
+
+ private:
+  R (T::*f_)(A1, A2, A3) const;
+  T* t_;
+};
+
+template <class T, class R, class A1, class A2, class A3, class A4>
+class Method<R(T::*)(A1, A2, A3, A4) const>
+    : public Callable<R(A1, A2, A3, A4)> {
+ public:
+  Method(R (T::*f)(A1, A2, A3, A4) const, T* t) : f_(f), t_(t) {}
+
+  virtual R operator()(A1 a1, A2 a2, A3 a3, A4 a4) const {
+    return (t_->*f_)(a1, a2, a3, a4);
+  }
+
+ private:
+  R (T::*f_)(A1, A2, A3, A4) const;
+  T* t_;
+};
+
+}  // namespace base
+
+#endif  // BASE_METHOD_H_
+
