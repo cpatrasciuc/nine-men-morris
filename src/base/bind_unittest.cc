f29b610a51867ebc930c1128299dfd19b36ccec3
Cristian Patrasciuc <cristian.patrasciuc@gmail.com>
2012-10-04 13:45:50 +0300

    Separate the unittests for arity 3 and the ones for pointers

--- src/base/bind_unittest.cc
+++ src/base/bind_unittest.cc
@@ -16,8 +16,17 @@ using std::string;
 
 int f1(int x) { return x; }
 int f2(int x, int y) { return x * y; }
-int f3(int* x, int* y, int z) { return (*x) * (*y) * z; }
+int f3(int x, int y, int z) { return x * y * z; }
 int f4(int x, int y, int z, int t) { return x * y * z * t; }
+
+int p2(int* x, int* y) { return (*x) * (*y); }
+
+class Helper {
+ public:
+  string test_method(string s, int index) {
+    return s.substr(index);
+  }
+};
 
 TEST(BindTest, Arity1) {
   Function<int(int)> func(&f1);
@@ -38,26 +47,19 @@ TEST(BindTest, Arity2) {
 }
 
 TEST(BindTest, Arity3) {
-  Function<int(int*, int*, int)> func3(&f3);
-  int a = 1;
-  int b = 2;
-  Callable<int(int)>* multiply_by_b = Bind(&func3, &a, &b);
-  EXPECT_EQ(10, (*multiply_by_b)(5));
-  b = 3;
-  EXPECT_EQ(15, (*multiply_by_b)(5));
-  delete multiply_by_b;
+  Function<int(int, int, int)> func3(&f3);
 
-  a = 2;
-  b = 3;
-  Callable<int(int*, int)>* double_product = Bind(&func3, &a);
-  EXPECT_EQ(24, (*double_product)(&b, 4));
-  delete double_product;
+  Callable<int(int, int)>* product = Bind(&func3, 1);
+  EXPECT_EQ(24, (*product)(6, 4));
+  delete product;
 
-  Callable<int(void)>* a_times_b = Bind(&func3, &a, &b, 1);
-  a = 5;
-  b = 2;
-  EXPECT_EQ(a * b, (*a_times_b)());
-  delete a_times_b;
+  Callable<int(int)>* multiply_by_2 = Bind(&func3, 1, 2);
+  EXPECT_EQ(10, (*multiply_by_2)(5));
+  delete multiply_by_2;
+
+  Callable<int(void)>* constant_function = Bind(&func3, 1, 2, -1);
+  EXPECT_EQ(-2, (*constant_function)());
+  delete constant_function;
 }
 
 TEST(BindTest, Arity4) {
@@ -80,26 +82,33 @@ TEST(BindTest, Arity4) {
   delete constant_function;
 }
 
+TEST(BindTest, Pointers) {
+  int a;
+  int b;
+  Function<int(int*, int*)> product2(&p2);
+  Callable<int(void)>* a_times_b = Bind(&product2, &a, &b);
+  a = 2;
+  b = 3;
+  EXPECT_EQ(a * b, (*a_times_b)());
+  a = 5;
+  b = -1;
+  EXPECT_EQ(a * b, (*a_times_b)());
+  delete a_times_b;
+}
+
 TEST(BindTest, Currying) {
   Function<int(int, int)> func2(&f2);
   // TODO(bind): Implement an automatic deletion mechanism for binders
-  Callable<int(int)>* tmp = Bind(&func2, 2);
-  Callable<int(void)>* final = Bind(tmp, 3);
-  EXPECT_EQ(6, (*final)());
-  delete tmp;
-  delete final;
+  Callable<int(int)>* simple_bind = Bind(&func2, 2);
+  Callable<int(void)>* composed_bind = Bind(simple_bind, 3);
+  EXPECT_EQ(6, (*composed_bind)());
+  delete simple_bind;
+  delete composed_bind;
 }
-
-class Helper {
- public:
-  string m2(string s, int index) {
-    return s.substr(index);
-  }
-};
 
 TEST(BindTest, Methods) {
   Helper h;
-  Method<string(Helper::*)(string, int)> m(&Helper::m2, &h);
+  Method<string(Helper::*)(string, int)> m(&Helper::test_method, &h);
   Callable<string(int)>* c = Bind(&m, string("foobar"));
   EXPECT_EQ("bar", (*c)(3));
   delete c;
