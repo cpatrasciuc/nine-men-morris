06c5bbccb4b13a3169ab965494696ca41071a94b
Cristian Patrasciuc <cristian.patrasciuc@gmail.com>
2012-10-16 17:26:27 +0300

    Add type-parametrized tests for Atomic<T> to cover all values of T

--- src/base/threading/atomic_unittest.cc
+++ src/base/threading/atomic_unittest.cc
@@ -2,6 +2,8 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
+#include <stdint.h>
+
 #include "base/threading/atomic.h"
 #include "gtest/gtest.h"
 
@@ -9,47 +11,57 @@ namespace base {
 namespace threading {
 namespace {
 
-// TODO(threading): Test all possible atomic types
+template <typename T>
+class AtomicTest : public ::testing::Test {};
 
-TEST(Atomic, DefaultInitialization) {
-  Atomic<int> atomic;
-  EXPECT_EQ(0, atomic.Get());
+typedef ::testing::Types<int8_t,  uint8_t,
+                         int16_t, uint16_t,
+                         int32_t, uint32_t,
+                         int64_t, uint64_t> AllowedAtomicTypes;
+TYPED_TEST_CASE(AtomicTest, AllowedAtomicTypes);
+
+TYPED_TEST(AtomicTest, DefaultInitialization) {
+  Atomic<TypeParam> atomic;
+  EXPECT_EQ(TypeParam(0), atomic.Get());
 }
 
-TEST(Atomic, Copy) {
-  Atomic<int> atomic1(10);
-  Atomic<int> atomic2(atomic1);
+TYPED_TEST(AtomicTest, Copy) {
+  Atomic<TypeParam> atomic1(10);
+  Atomic<TypeParam> atomic2(atomic1);
   EXPECT_EQ(atomic1.Get(), atomic2.Get());
 }
 
-TEST(Atomic, BasicOperations) {
-  Atomic<int> atomic(2);
+TYPED_TEST(AtomicTest, BasicOperations) {
+  Atomic<TypeParam> atomic(2);
+  EXPECT_EQ(TypeParam(2), atomic.Get());
   atomic.Add(5);
-  EXPECT_EQ(7, atomic.Get());
+  EXPECT_EQ(TypeParam(7), atomic.Get());
   atomic.Subtract(3);
-  EXPECT_EQ(4, atomic.Get());
+  EXPECT_EQ(TypeParam(4), atomic.Get());
   atomic.Increment();
-  EXPECT_EQ(5, atomic.Get());
+  EXPECT_EQ(TypeParam(5), atomic.Get());
   atomic.Decrement();
-  EXPECT_EQ(4, atomic.Get());
+  EXPECT_EQ(TypeParam(4), atomic.Get());
   atomic.BitwiseAnd(4);
-  EXPECT_EQ(4, atomic.Get());
+  EXPECT_EQ(TypeParam(4), atomic.Get());
   atomic.BitwiseAnd(1);
-  EXPECT_EQ(0, atomic.Get());
+  EXPECT_EQ(TypeParam(0), atomic.Get());
   atomic.BitwiseOr(0);
-  EXPECT_EQ(0, atomic.Get());
+  EXPECT_EQ(TypeParam(0), atomic.Get());
   atomic.BitwiseOr(10);
-  EXPECT_EQ(10, atomic.Get());
+  EXPECT_EQ(TypeParam(10), atomic.Get());
   atomic.BitwiseXor(12);
-  EXPECT_EQ(6, atomic.Get());
-  // TODO(clang): Enable this when clang version is updated
-  /*atomic.BitwiseNand(1);
-  EXPECT_EQ(1, atomic.Get());*/
+  EXPECT_EQ(TypeParam(6), atomic.Get());
+  // TODO(clang): Enable this when clang supports atomic nand
+  // atomic.BitwiseNand(1);
+  // EXPECT_EQ(TypeParam(1), atomic.Get());
   atomic.CompareAndSwap(10, 10);
-  EXPECT_EQ(6, atomic.Get());
+  EXPECT_EQ(TypeParam(6), atomic.Get());
   atomic.CompareAndSwap(6, 10);
-  EXPECT_EQ(10, atomic.Get());
+  EXPECT_EQ(TypeParam(10), atomic.Get());
 }
+
+// TODO(threading): Write tests for Atomic<bool> if supported.
 
 }  // anonymous namespace
 }  // namespace threading
