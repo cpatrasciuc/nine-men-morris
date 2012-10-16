35bbda9ad9d7819d41a6b3e0d7f462ec16f3331d
Cristian Patrasciuc <cristian.patrasciuc@gmail.com>
2012-10-16 18:15:18 +0300

    Added a counting test that uses an atomic variable instead of locks

--- src/base/threading/thread_unittest.cc
+++ src/base/threading/thread_unittest.cc
@@ -6,8 +6,10 @@
 
 #include "base/bind.h"
 #include "base/location.h"
+#include "base/log.h"
 #include "base/method.h"
 #include "base/string_util.h"
+#include "base/threading/atomic.h"
 #include "base/threading/lock.h"
 #include "base/threading/thread.h"
 #include "gtest/gtest.h"
@@ -16,52 +18,85 @@ namespace base {
 namespace threading {
 namespace {
 
-class Counter {
+const int kThreadCount = 10;
+
+class LockCounter {
  public:
-  Counter() : counter_(0) {}
+  LockCounter() : counter_(0) {}
 
   void IncrementCounter() {
     ScopedGuard _(&counter_lock_);
     ++counter_;
   }
 
-  int counter() const {
-    return counter_;
-  }
+  int value() const { return counter_; }
 
  private:
-  // TODO(threading) : Replace this with atomic variable
   int counter_;
   Lock counter_lock_;
 };
 
-TEST(Thread, Basic) {
-  int n = 10;
-  Counter c;
-  std::vector<Thread*> threads;
+class AtomicCounter {
+ public:
+  AtomicCounter() : counter_(0) {}
+  void IncrementCounter() { counter_.Increment(); }
+  int value() const { return counter_.Get(); }
 
-  for (int i = 0; i < n; ++i) {
-    threads.push_back(new Thread("Test Thread " + ToString(i+1)));
+ private:
+  Atomic<int> counter_;
+};
+
+template <typename T>
+class ThreadTest : public ::testing::Test {
+ public:
+  virtual void SetUp() {
+    for (int i = 0; i < kThreadCount; ++i) {
+      threads_.push_back(new Thread("Test Thread " + ToString(i+1)));
+    }
+    for (size_t i = 0; i < threads_.size(); ++i) {
+      EXPECT_FALSE(threads_[i]->is_running());
+      threads_[i]->Start();
+      EXPECT_TRUE(threads_[i]->is_running());
+      EXPECT_FALSE(Thread::CurrentlyOn(threads_[i]));
+    }
   }
 
-  for (size_t i = 0; i < threads.size(); ++i) {
-    EXPECT_FALSE(threads[i]->is_running());
-    threads[i]->Start();
-    EXPECT_TRUE(threads[i]->is_running());
-    EXPECT_FALSE(Thread::CurrentlyOn(threads[i]));
+  virtual void TearDown() {
+    for (size_t i = 0; i < threads_.size(); ++i) {
+      threads_[i]->SubmitQuitTaskAndJoin();
+      delete threads_[i];
+    }
+    EXPECT_EQ(thread_count(), counter_.value());
   }
 
-  for (size_t i = 0; i < threads.size(); ++i) {
-    threads[i]->SubmitTask(FROM_HERE,
-      Bind(new Method<void(Counter::*)(void)>(&Counter::IncrementCounter), &c));
+  void SubmitTask(int thread_number, Location loc, Closure* closure) {
+    DCHECK_LT(thread_number, thread_count());
+    threads_[thread_number]->SubmitTask(loc, closure);
   }
 
-  for (size_t i = 0; i < threads.size(); ++i) {
-    threads[i]->SubmitQuitTaskAndJoin();
-    delete threads[i];
+ protected:
+  int thread_count() const {
+    return threads_.size();
   }
 
-  EXPECT_EQ(n, c.counter());
+  T* counter() {
+    return &counter_;
+  }
+
+ private:
+  std::vector<Thread*> threads_;
+  T counter_;
+};
+
+typedef ::testing::Types<LockCounter, AtomicCounter> CounterTypes;
+TYPED_TEST_CASE(ThreadTest, CounterTypes);
+
+TYPED_TEST(ThreadTest, Counting) {
+  for (int i = 0; i < this->thread_count(); ++i) {
+    this->SubmitTask(i, FROM_HERE,
+        Bind(new Method<void(TypeParam::*)(void)>(&TypeParam::IncrementCounter),
+            this->counter()));
+  }
 }
 
 }  // anonymous namespace
