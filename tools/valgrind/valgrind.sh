2b4b8ea9640467e27652dc3d30bc3556abe881ba
Cristian Patrasciuc (Home) <cristian.patrasciuc@gmail.com>
2012-10-21 18:05:50 +0300

    Speed up valgrind tests by not tracking the origins

--- tools/valgrind/valgrind.sh
+++ tools/valgrind/valgrind.sh
@@ -4,6 +4,5 @@ export GTEST_DEATH_TEST_USE_FORK=1
 valgrind --gen-suppressions=yes \
 	--suppressions=`dirname $0`/suppressions.txt \
 	--leak-check=full \
-	--track-origins=yes \
 	--show-reachable=yes \
 	$@
