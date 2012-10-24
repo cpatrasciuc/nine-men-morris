0389447ed5cd7e9e78b312dc6e4f67e3bb1c4dd1
Cristian Patrasciuc <cristian.patrasciuc@gmail.com>
2012-10-24 18:40:50 +0300

    Remove the need to manually update the location unit test.

--- src/base/location_unittest.cc
+++ src/base/location_unittest.cc
@@ -10,15 +10,20 @@
 namespace base {
 namespace {
 
-Location GetTestLocation() {
-  return FROM_HERE;
-}
+// Force the preprocessor to consider this the 20th line so we don't have to
+// update the unit test every time we make changes to this file.
+//
+// WARNING:
+//   This also generates incorrect line numbers in stack traces for this file.
+#define TEST_LINE_NUMBER 20
+#line TEST_LINE_NUMBER
+Location GetTestLocation() { return FROM_HERE; }
 
 TEST(LocationTest, FromHere) {
   Location loc = GetTestLocation();
   EXPECT_EQ("GetTestLocation", loc.function());
   EXPECT_EQ("location_unittest.cc", loc.file_name());
-  EXPECT_EQ(14, loc.line_number());
+  EXPECT_EQ(TEST_LINE_NUMBER, loc.line_number());
 }
 
 }  // anonymous namespace
