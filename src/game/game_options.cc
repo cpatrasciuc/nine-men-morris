6e1dc64bc487a1133fd0ce4c9c1f5b15a9b3c411
Cristian Patrasciuc (Home) <cristian.patrasciuc@gmail.com>
2012-10-28 15:32:47 +0200

    Initial commit for a class that stores the game options.

---
+++ src/game/game_options.cc
@@ -0,0 +1,15 @@
+// Copyright (c) 2012 Cristian Patrasciuc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "game/game_options.h"
+
+namespace game {
+
+GameOptions::GameOptions()
+    : game_type_(NINE_MEN_MORRIS),
+      jumps_allowed_(true),
+      white_starts_(true) {
+}
+
+}  // namespace game
---
+++ src/game/game_options.h
@@ -0,0 +1,50 @@
+// Copyright (c) 2012 Cristian Patrasciuc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef GAME_GAME_OPTIONS_H_
+#define GAME_GAME_OPTIONS_H_
+
+#include "game/game_export.h"
+
+namespace game {
+
+// Class used to group together all the configurable parameters of a game.
+class GAME_EXPORT GameOptions {
+ public:
+  // Enumeration that specifies all the available game configurations.
+  enum GameType {
+    THREE_MEN_MORRIS,
+    SIX_MEN_MORRIS,
+    NINE_MEN_MORRIS,
+    // TODO(game): TWELVE_MEN_MORRIS
+  };
+
+  // Initializes a new object with the default game options.
+  GameOptions();
+
+  // Specifies one of the allowed board setups. Default: NINE_MEN_MORRIS.
+  GameType game_type() const { return game_type_; }
+  void set_game_type(GameType type) { game_type_ = type; }
+
+  // Specifies if jumps are allowed when a player remains with less then three
+  // pieces on the board. Default: true.
+  bool jumps_allowed() const { return jumps_allowed_; }
+  void set_jumps_allowed(bool allow) { jumps_allowed_ = allow; }
+
+  // Specifies which player is the first to move. If |white_starts_| is true,
+  // the white player will move first, otherwise the black player will move
+  // first. Default: true.
+  bool white_starts() const { return white_starts_; }
+  void set_white_starts(bool white_starts) { white_starts_ = white_starts; }
+
+ private:
+  GameType game_type_;
+  bool jumps_allowed_;
+  bool white_starts_;
+};
+
+}  // namespace game
+
+#endif  // GAME_GAME_OPTIONS_H_
+
