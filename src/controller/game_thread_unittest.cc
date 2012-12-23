e6643e2bc2ddb973e72c0a6eac918bdf67b9f45f
Cristian Patrasciuc <cristian.patrasciuc@gmail.com>
2012-12-23 14:42:50 +0200

    Add a class that manages the game threads.

---
+++ src/controller/game_thread_unittest.cc
@@ -0,0 +1,21 @@
+// Copyright (c) 2012 Cristian Patrasciuc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "base/threading/thread.h"
+#include "controller/game_thread.h"
+#include "gtest/gtest.h"
+
+namespace controller {
+namespace {
+
+TEST(GameThread, Basic) {
+  GameThread::SetUpGameThreads();
+  for (int id = 0; id < GameThread::ID_COUNT; ++id) {
+    GameThread::Get(static_cast<GameThread::ID>(id)).QuitWhenIdle();
+  }
+  GameThread::JoinGameThreads();
+}
+
+}  // anonymous namespace
+}  // namespace controller
