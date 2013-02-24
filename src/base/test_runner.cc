cd95d0d7ac70f665a60ae154fb80b52acce1d677
Cristian Patrasciuc <cristian.patrasciuc@hp.com>
2013-02-24 11:45:39 +0200

    Removed out-of-date TODO item.

--- src/base/test_runner.cc
+++ src/base/test_runner.cc
@@ -2,8 +2,6 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-// TODO(cpatrasciuc): Auto-generated this file when a test target is built.
-
 #include "base/debug/stacktrace.h"
 #include "gtest/gtest.h"
 
