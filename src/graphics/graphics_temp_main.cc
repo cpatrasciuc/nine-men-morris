0dfb3a504637de11a4dc06b18beaf8668025df0e
Cristian Patrasciuc (Home) <cristian.patrasciuc@gmail.com>
2013-03-11 23:19:29 +0200

    Add temporary test executable for the graphics subfolder.

---
+++ src/graphics/graphics_temp_main.cc
@@ -0,0 +1,10 @@
+// Copyright (c) 2013 Cristian Patrasciuc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include "graphics/ogre_app.h"
+
+int main(int argc, char** argv) {
+  graphics::OgreApp app;
+  return app.RunMainLoop();
+}
