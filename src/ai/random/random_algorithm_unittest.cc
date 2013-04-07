460a4f03f61a7118ed3401fd8d0efc506387e839
Cristian Patrasciuc <cristian.patrasciuc@hp.com>
2013-04-07 03:26:47 +0300

    Updated the random AI test to use the base::MersenneTwister32 generator

--- src/ai/random/random_algorithm_unittest.cc
+++ src/ai/random/random_algorithm_unittest.cc
@@ -4,11 +4,15 @@
 
 #include <cstdlib>
 #include <ctime>
+#include <memory>
 
 #include "ai/ai_algorithm.h"
 #include "ai/random/random_algorithm.h"
+#include "base/bind.h"
+#include "base/function.h"
 #include "base/log.h"
 #include "base/ptr/scoped_ptr.h"
+#include "base/random.h"
 #include "game/game.h"
 #include "game/game_options.h"
 #include "game/piece_color.h"
@@ -19,27 +23,38 @@ namespace ai {
 namespace random {
 namespace {
 
+int GetNextRandomAsInt(base::MersenneTwister32* generator) {
+  return static_cast<int>(generator->Next());
+}
+
+RandomAlgorithm* GetRandomTestPlayer(base::MersenneTwister32* generator) {
+  base::Function<int(base::MersenneTwister32*)>* wrapper =
+      new base::Function<int(base::MersenneTwister32*)>(&GetNextRandomAsInt);
+  std::auto_ptr<RandomAlgorithm::RandomNumberGenerator> bind_ptr(
+      base::Bind(wrapper, generator));
+  return new RandomAlgorithm(bind_ptr);
+}
+
 TEST(RandomAlgorithm, ThreeMenMorrisFullGame) {
-  // TODO(random_algorithm): Replace this random generator with a stable one
   const int max_moves = 100;
-  base::ptr::scoped_ptr<AIAlgorithm> white(new RandomAlgorithm());
-  base::ptr::scoped_ptr<AIAlgorithm> black(new RandomAlgorithm());
+  base::MersenneTwister32 white_random(12345);
+  base::MersenneTwister32 black_random(54321);
+  base::ptr::scoped_ptr<AIAlgorithm> white(GetRandomTestPlayer(&white_random));
+  base::ptr::scoped_ptr<AIAlgorithm> black(GetRandomTestPlayer(&black_random));
   game::GameOptions game_options;
   game_options.set_game_type(game::GameOptions::THREE_MEN_MORRIS);
   game::Game test_game(game_options);
   test_game.Initialize();
   for (int i = 0; i < max_moves; ++i) {
-    AIAlgorithm* next_player = Get(
-        (test_game.current_player() == game::WHITE_COLOR) ? white : black);
+    AIAlgorithm* next_player =
+        Get((test_game.current_player() == game::WHITE_COLOR) ? white : black);
     game::PlayerAction action(next_player->GetNextAction(test_game));
     test_game.ExecutePlayerAction(action);
     if (test_game.is_game_over()) {
-      // TODO(random_algorithm): Assert that game is over once we have a stable
-      // random number generator.
-      LOG(INFO) << "Game is over";
       break;
     }
   }
+  EXPECT_TRUE(test_game.is_game_over());
 }
 
 }  // anonymous namespace
