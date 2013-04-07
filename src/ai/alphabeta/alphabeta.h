3c8a61fcf5b94e62d02828358a768ac4c7c5f1a5
Cristian Patrasciuc <cristian.patrasciuc@hp.com>
2013-04-08 02:51:09 +0300

    Added a generic implementation of the Alpha Beta Pruning algorithm.

---
+++ src/ai/alphabeta/alphabeta.h
@@ -0,0 +1,128 @@
+// Copyright (c) 2013 Cristian Patrasciuc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#ifndef AI_ALPHABETA_ALPHABETA_H_
+#define AI_ALPHABETA_ALPHABETA_H_
+
+#include <limits>
+#include <map>
+#include <utility>
+#include <vector>
+
+#include "base/basic_macros.h"
+#include "base/ptr/scoped_ptr.h"
+
+namespace ai {
+namespace alphabeta {
+
+// This represents a generic implementation of the Alpha Beta Pruning algorithm.
+// The template is based on two external classes. The |State| class must be an
+// encoding of a game state. It is extensively used throughout the search so
+// operations like copying or testing for equality should not take too much
+// time. The |Score| class is used to store the result of evaluating the
+// states corresponding to the leafs of the partially constructed game tree.
+template <class State, class Score = int>
+class AlphaBeta {
+ public:
+  // In order to get the successors of a given state or to evaluate leaf states,
+  // the users of this class must provide an implementation of this |Delegate|
+  // interface that is tailored to their specific game.
+  class Delegate {
+   public:
+    virtual ~Delegate() {}
+
+    // This method should return |true| if the given |state| is one of the
+    // game terminal/ending states.
+    virtual bool IsTerminal(const State& state) = 0;
+
+    // This method must return the score of the given |state|. The |state| is
+    // not mandatory to be a terminal one. The second argument specifies if the
+    // evaluation should be performed from the max_player point of view or not.
+    // NOTE: max_player is the player that seeks to maximize the score.
+    virtual Score Evaluate(const State& state, bool max_player) = 0;
+
+    // This method must fill in the |successors| vector with the successors of
+    // the state given as first argument.
+    virtual void GetSuccessors(const State& state,
+                               std::vector<State>* successors) = 0;
+
+   protected:
+    Delegate() {}
+
+   private:
+    DISALLOW_COPY_AND_ASSIGN(Delegate);
+  };
+
+  // The constructor receives as argument a Delegate instance that is used to
+  // adapt the Alpha Beta Pruning algorithm to a specific game.
+  explicit AlphaBeta(std::auto_ptr<Delegate> delegate)
+      : delegate_(delegate.release()), best_successor_() {}
+
+  // Starts a search in the partially constructed game tree that is rooted at
+  // the state given by the |origin| argument and goes down in the tree for at
+  // most |depth| levels. The method returns the best successor state that it
+  // found. It is the responsibility of the users of this method to determine
+  // the actual move that gets them from |origin| to that specific state.
+  State GetBestSuccessor(const State& origin, int depth) {
+    best_successor_ = State();
+    const Score min_infinity = std::numeric_limits<Score>::min();
+    const Score max_infinity = std::numeric_limits<Score>::max();
+    Search(origin, depth, min_infinity, max_infinity, true, true);
+    return best_successor_;
+  };
+
+ private:
+  // The core of the Alpha Beta Pruning search algorithm. For more details read
+  // http://en.wikipedia.org/wiki/Alpha-beta_pruning.
+  Score Search(const State& state, int depth, Score alpha, Score beta,
+               bool max_player, bool store_best_successor = false) {
+    if (depth == 0 || delegate_->IsTerminal(state)) {
+      return delegate_->Evaluate(state, max_player);
+    }
+    std::vector<State> successors;
+    delegate_->GetSuccessors(state, &successors);
+    if (max_player) {
+      for (size_t i = 0; i < successors.size(); ++i) {
+        Score s = Search(successors[i], depth - 1, alpha, beta, !max_player);
+        if (s > alpha) {
+          alpha = s;
+          if (store_best_successor) {
+            best_successor_ = successors[i];
+          }
+        }
+        if (beta <= alpha) {
+          break;
+        }
+      }
+      return alpha;
+    }
+    for (size_t i = 0; i < successors.size(); ++i) {
+      Score s = Search(successors[i], depth - 1, alpha, beta, !max_player);
+      if (s < beta) {
+        beta = s;
+        if (store_best_successor) {
+          best_successor_ = successors[i];
+        }
+      }
+      if (beta <= alpha) {
+        break;
+      }
+    }
+    return beta;
+  };
+
+  base::ptr::scoped_ptr<Delegate> delegate_;
+
+  // This variable is used to store the temporary best successor during one
+  // search (i.e. during one call to |GetBestSuccessor()|).
+  State best_successor_;
+
+  DISALLOW_COPY_AND_ASSIGN(AlphaBeta);
+};
+
+}  // namespace alphabeta
+}  // namespace ai
+
+#endif  // AI_ALPHABETA_ALPHABETA_H_
+
---
+++ src/ai/alphabeta/alphabeta_unittest.cc
@@ -0,0 +1,104 @@
+// Copyright (c) 2013 Cristian Patrasciuc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <memory>
+
+#include "ai/alphabeta/alphabeta.h"
+#include "gtest/gtest.h"
+
+namespace ai {
+namespace alphabeta {
+namespace {
+
+// This test simulates an abstract game whose game tree expansion would be
+// equalt to the one from the following image:
+// http://upload.wikimedia.org/wikipedia/commons/9/91/AB_pruning.svg.
+// The state encoding is an integer representing the 0-based index of the node
+// in the BFS traversal of the tree in the picture. The root will have index 0.
+// The children of one node are visited in left-to-right order.
+// The score is a |double| only for testing purposes. The values are only given
+// for the leaf nodes in the picture.
+class TestDelegate : public AlphaBeta<int, double>::Delegate {
+ public:
+  TestDelegate() {
+    children_[0].push_back(1);
+    children_[0].push_back(2);
+    children_[0].push_back(3);
+    children_[1].push_back(4);
+    children_[1].push_back(5);
+    children_[2].push_back(6);
+    children_[2].push_back(7);
+    children_[3].push_back(8);
+    children_[3].push_back(9);
+    children_[4].push_back(10);
+    children_[4].push_back(11);
+    children_[5].push_back(12);
+    children_[6].push_back(13);
+    children_[6].push_back(14);
+    children_[7].push_back(15);
+    children_[8].push_back(16);
+    children_[9].push_back(17);
+    children_[9].push_back(18);
+    children_[10].push_back(19);
+    children_[10].push_back(20);
+    children_[11].push_back(21);
+    children_[11].push_back(22);
+    children_[11].push_back(23);
+    children_[12].push_back(24);
+    children_[13].push_back(25);
+    children_[14].push_back(26);
+    children_[14].push_back(27);
+    children_[15].push_back(28);
+    children_[16].push_back(29);
+    children_[17].push_back(30);
+    children_[17].push_back(31);
+    children_[18].push_back(32);
+
+    scores_[19] = 5;
+    scores_[20] = 6;
+    scores_[21] = 7;
+    scores_[22] = 4;
+    scores_[23] = 5;
+    scores_[24] = 3;
+    scores_[25] = 6;
+    scores_[26] = 6;
+    scores_[27] = 9;
+    scores_[28] = 7;
+    scores_[29] = 5;
+    scores_[30] = 9;
+    scores_[31] = 8;
+    scores_[32] = 6;
+  }
+
+ private:
+  virtual bool IsTerminal(const int& state) {
+    return scores_.count(state) > 0;
+  };
+
+  virtual double Evaluate(const int& state, bool max_player) {
+    EXPECT_TRUE(max_player);
+    EXPECT_TRUE(IsTerminal(state));
+    return scores_[state];
+  }
+
+  virtual void GetSuccessors(const int& state, std::vector<int>* successors) {
+    // TODO(alphabeta): Mark the visited states and verifies at the end
+    ASSERT_GT(children_.count(state), 0U);
+    const std::vector<int>& children = children_[state];
+    successors->insert(successors->end(), children.begin(), children.end());
+  }
+
+  std::map<int, std::vector<int> > children_;
+  std::map<int, double> scores_;
+};
+
+TEST(AlphaBeta, AbstractGameTree) {
+  AlphaBeta<int, double> alpha_beta(
+      std::auto_ptr<AlphaBeta<int, double>::Delegate>(new TestDelegate()));
+  EXPECT_EQ(2, alpha_beta.GetBestSuccessor(0, 1000));
+}
+
+}  // anonymous namespace
+}  // namespace alphabeta
+}  // namespace ai
