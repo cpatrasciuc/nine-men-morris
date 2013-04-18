3bc00f225e975b0b0c3adeab73e8ad7220b66966
Cristian Patrasciuc <cristian.patrasciuc@hp.com>
2013-04-18 19:52:25 +0300

    Extract the code that filters board location to a separate function.

--- src/ai/game_state_generator.cc
+++ src/ai/game_state_generator.cc
@@ -14,6 +14,42 @@
 
 namespace ai {
 
+namespace {
+
+void FilterBoardLocations(const game::Board& board,
+                          const game::PieceColor player_color,
+                          std::vector<game::BoardLocation>* player,
+                          std::vector<game::BoardLocation>* removable,
+                          std::vector<game::BoardLocation>* empty) {
+  std::vector<game::BoardLocation> opponent;
+  std::vector<game::BoardLocation> mills;
+  for (int line = 0; line < board.size(); ++line) {
+    for (int col = 0; col < board.size(); ++col) {
+      const game::BoardLocation loc(line, col);
+      if (!board.IsValidLocation(loc)) {
+        continue;
+      }
+      const game::PieceColor loc_color = board.GetPieceAt(loc);
+      if (loc_color == game::NO_COLOR) {
+        empty->push_back(loc);
+      } else if (loc_color == player_color) {
+        player->push_back(loc);
+      } else if (board.IsPartOfMill(loc)) {
+        mills.push_back(loc);
+      } else {
+        opponent.push_back(loc);
+      }
+    }
+  }
+  if (opponent.empty()) {
+    removable->insert(removable->end(), mills.begin(), mills.end());
+  } else {
+    removable->insert(removable->end(), opponent.begin(), opponent.end());
+  }
+}
+
+}  // anonymous namespace
+
 GameStateGenerator::GameStateGenerator(const game::GameOptions& game_options)
     : game_options_(game_options) {}
 
@@ -63,49 +99,28 @@ void GameStateGenerator::GetMoveSuccessors(const GameState& state,
                                            std::vector<GameState>* successors) {
   game::Board board(game_options_.game_type());
   state.Decode(&board);
-  std::vector<game::BoardLocation> player_locations;
-  std::vector<game::BoardLocation> empty_locations;
-  std::vector<game::BoardLocation> opponent_locations;
-  std::vector<game::BoardLocation> morris_locations;
+  std::vector<game::BoardLocation> player_loc;
+  std::vector<game::BoardLocation> empty_loc;
+  std::vector<game::BoardLocation> opponent_loc;
   const game::PieceColor player = state.current_player();
   const game::PieceColor opponent = game::GetOpponent(player);
-  for (int line = 0; line < board.size(); ++line) {
-    for (int col = 0; col < board.size(); ++col) {
-      const game::BoardLocation loc(line, col);
-      if (!board.IsValidLocation(loc)) {
-        continue;
-      }
-      const game::PieceColor loc_color = board.GetPieceAt(loc);
-      if (loc_color == game::NO_COLOR) {
-        empty_locations.push_back(loc);
-      } else if (loc_color == player) {
-        player_locations.push_back(loc);
-      } else if (board.IsPartOfMill(loc)) {
-        morris_locations.push_back(loc);
-      } else {
-        opponent_locations.push_back(loc);
-      }
-    }
-  }
-  if (opponent_locations.empty()) {
-    opponent_locations = morris_locations;
-  }
-  for (size_t i = 0; i < player_locations.size(); ++i) {
-    for (size_t j = 0; j < empty_locations.size(); ++j) {
-      if (!board.IsAdjacent(player_locations[i], empty_locations[j])) {
-        if (player_locations.size() > 3 || !game_options_.jumps_allowed()) {
+  FilterBoardLocations(board, player, &player_loc, &opponent_loc, &empty_loc);
+  for (size_t i = 0; i < player_loc.size(); ++i) {
+    for (size_t j = 0; j < empty_loc.size(); ++j) {
+      if (!board.IsAdjacent(player_loc[i], empty_loc[j])) {
+        if (player_loc.size() > 3 || !game_options_.jumps_allowed()) {
           continue;
         }
       }
-      board.MovePiece(player_locations[i], empty_locations[j]);
-      if (board.IsPartOfMill(empty_locations[j])) {
-        for (size_t k = 0; k < opponent_locations.size(); ++k) {
-          board.RemovePiece(opponent_locations[k]);
+      board.MovePiece(player_loc[i], empty_loc[j]);
+      if (board.IsPartOfMill(empty_loc[j])) {
+        for (size_t k = 0; k < opponent_loc.size(); ++k) {
+          board.RemovePiece(opponent_loc[k]);
           GameState successor;
           successor.set_current_player(opponent);
           successor.Encode(board);
           successors->push_back(successor);
-          board.AddPiece(opponent_locations[k], opponent);
+          board.AddPiece(opponent_loc[k], opponent);
         }
       } else {
         GameState successor;
@@ -113,7 +128,7 @@ void GameStateGenerator::GetMoveSuccessors(const GameState& state,
         successor.Encode(board);
         successors->push_back(successor);
       }
-      board.MovePiece(empty_locations[j], player_locations[i]);
+      board.MovePiece(empty_loc[j], player_loc[i]);
     }
   }
 }
