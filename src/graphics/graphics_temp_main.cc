02ddd5b46f4aec8b056d5226e67c93a1b997efeb
Cristian Patrasciuc (Home) <cristian.patrasciuc@gmail.com>
2013-05-15 23:41:12 +0300

    Allow testing for multiple game types.

--- src/graphics/graphics_temp_main.cc
+++ src/graphics/graphics_temp_main.cc
@@ -2,11 +2,13 @@
 // Use of this source code is governed by a BSD-style license that can be
 // found in the LICENSE file.
 
-#include <OIS/OISKeyboard.h>
+#include "game/game_options.h"
 
 #include "graphics/ogre_app.h"
 #include "graphics/game_state.h"
 #include "graphics/playing_state.h"
+
+#include "OIS/OISKeyboard.h"
 
 namespace {
 
@@ -27,8 +29,10 @@ class EmptyGameState : public graphics::GameState {
 int main(int argc, char** argv) {
   graphics::OgreApp app("Temp test for OgreApp");
   app.Init();
+  game::GameOptions options;
+  options.set_game_type(game::GameOptions::SIX_MEN_MORRIS);
   // EmptyGameState game_state(&app);
-  graphics::PlayingState game_state(&app);
+  graphics::PlayingState game_state(&app, options);
   app.PushState(&game_state);
   app.RunMainLoop();
   return 0;
