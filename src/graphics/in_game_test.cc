a5caf4788e1176c29134e576e6d4ea194c1e6f31
Cristian Patrasciuc <calligrapha@gmail.com>
2013-06-29 21:31:22 +0300

    Add a "trampoline" feature to the InGameTestBase class.

--- src/graphics/in_game_test.cc
+++ src/graphics/in_game_test.cc
@@ -5,12 +5,15 @@
 #include "graphics/in_game_test.h"
 
 #include "base/basic_macros.h"
+#include "base/ptr/scoped_ptr.h"
 #include "graphics/ogre_app.h"
 #include "graphics/game_state.h"
 #include "gtest/gtest.h"
 
+#include "OGRE/OgreAny.h"
 #include "OGRE/OgreFrameListener.h"
 #include "OGRE/OgreRoot.h"
+#include "OGRE/OgreWorkQueue.h"
 
 namespace graphics {
 namespace {
@@ -37,21 +40,50 @@ class RunTestMethodGameState : public GameState {
 
 }  // anonymous namespace
 
-InGameTestBase::InGameTestBase() : app_("Game Tests"), first_state_(NULL) {}
+InGameTestBase::InGameTestBase()
+    : app_("Game Tests"), first_state_(NULL), channel_(-1) {}
 
 InGameTestBase::~InGameTestBase() {}
 
 void InGameTestBase::SetUp() {
   // TODO(game_test): Provide an ogre config file for test.
   ASSERT_TRUE(app_.Init());
+  Ogre::WorkQueue* const work_queue = Ogre::Root::getSingleton().getWorkQueue();
+  channel_ = Ogre::Root::getSingleton().getWorkQueue()->getChannel("GameTest");
+  work_queue->addRequestHandler(channel_, this);
+  work_queue->addResponseHandler(channel_, this);
   Reset(first_state_, new RunTestMethodGameState(this));
   app_.PushState(Get(first_state_));
+}
+
+void InGameTestBase::TearDown() {
+  Ogre::WorkQueue* const work_queue = Ogre::Root::getSingleton().getWorkQueue();
+  work_queue->removeRequestHandler(channel_, this);
+  work_queue->removeResponseHandler(channel_, this);
 }
 
 void InGameTestBase::Done() {
   ASSERT_TRUE(Get(first_state_));
   ASSERT_EQ(Get(first_state_), app_.PopState());
   Reset(first_state_);
+}
+
+void InGameTestBase::PostTaskOnGameLoop(base::Closure* task) {
+  Ogre::WorkQueue* const work_queue = Ogre::Root::getSingleton().getWorkQueue();
+  work_queue->addRequest(channel_, 0, Ogre::Any(task));
+}
+
+Ogre::WorkQueue::Response* InGameTestBase::handleRequest(
+    const Ogre::WorkQueue::Request* request,
+    const Ogre::WorkQueue* source_queue) {
+  return OGRE_NEW Ogre::WorkQueue::Response(request, true, Ogre::Any());
+}
+
+void InGameTestBase::handleResponse(const Ogre::WorkQueue::Response* response,
+                                    const Ogre::WorkQueue* source_queue) {
+  base::ptr::scoped_ptr<base::Closure> task(
+      response->getRequest()->getData().get<base::Closure*>());
+  (*task)();
 }
 
 }  // namespace graphics
--- src/graphics/in_game_test.h
+++ src/graphics/in_game_test.h
@@ -6,14 +6,20 @@
 #define GRAPHICS_IN_GAME_TEST_H_
 
 #include "base/basic_macros.h"
+#include "base/callable.h"
 #include "base/ptr/scoped_ptr.h"
 #include "graphics/game_state.h"
 #include "graphics/ogre_app.h"
 #include "gtest/gtest.h"
 
+#include "OGRE/OgreWorkQueue.h"
+
 namespace graphics {
 
-class InGameTestBase : public ::testing::Test {
+class InGameTestBase
+    : public ::testing::Test,
+      public Ogre::WorkQueue::RequestHandler,
+      public Ogre::WorkQueue::ResponseHandler {
  public:
   // Must be overwritten by specific test cases. This method will be called on
   // the game loop after the first frame is queued for rendering.
@@ -28,17 +34,34 @@ class InGameTestBase : public ::testing::Test {
   // Initializes |app_| and pushes |this| on the game states stack.
   virtual void SetUp();
 
+  virtual void TearDown();
+
   // Must be called when the test is over and the game loop can exit. It checks
   // that |this| is the only game state left on the stack and pops it.
   void Done();
 
+  // TODO(ogre_app): Move the "trampoline" feature to the OgreApp class.
+  void PostTaskOnGameLoop(base::Closure* task);
+
  private:
+  // Ogre::RequestHandler interface
+  virtual Ogre::WorkQueue::Response* handleRequest(
+      const Ogre::WorkQueue::Request* request,
+      const Ogre::WorkQueue* source_queue);
+
+  // Ogre::ResponseHandler interface
+  virtual void handleResponse(const Ogre::WorkQueue::Response* response,
+                              const Ogre::WorkQueue* source_queue);
+
   OgreApp app_;
 
   // The first game state that is pushed on the stack when the test is run. A
   // pointer to it is stored to ensure that when the Done() method is called and
   // the test is over it is the only state remaining on the stack.
   base::ptr::scoped_ptr<GameState> first_state_;
+
+  // Ogre work queue channel used to bounce back tasks on the UI thread.
+  Ogre::uint16 channel_;
 
   DISALLOW_COPY_AND_ASSIGN(InGameTestBase);
 };
