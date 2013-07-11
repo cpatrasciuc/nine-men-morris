3eb104b67228f7961dc6a0c0bf73aa9ea3fd69db
Cristian Patrasciuc (Home) <cristian.patrasciuc@gmail.com>
2013-07-11 22:12:17 +0300

    Add documentation for MenuState class.

--- src/graphics/menu_state.h
+++ src/graphics/menu_state.h
@@ -21,12 +21,21 @@ class Overlay;
 
 namespace graphics {
 
+// Generic class used to create menu screens.
 class GRAPHICS_EXPORT MenuState : public GameState {
  public:
+  // When creating a concrete menu screen, the client must provide a concrete
+  // delegate implementation that should perform the corresponding actions
+  // when a menu option is selected.
   class Delegate {
    public:
     virtual ~Delegate();
 
+    // This method is called when a menu options is selected. The menu option
+    // name is provided as argument.
+    // NOTE: The current implementation calls this method whenever an overlay
+    // element with a non-empty caption is clicked. The delegate should verify
+    // if this is indeed a valid menu option.
     virtual void OnMenuOptionSelected(const std::string& option) = 0;
 
    protected:
@@ -36,6 +45,9 @@ class GRAPHICS_EXPORT MenuState : public GameState {
     DISALLOW_COPY_AND_ASSIGN(Delegate);
   };
 
+  // Instantiates a new menu screen by providing a |menu_name| and a |delegate|.
+  // The |menu_name| must be the name of an OGRE overlay available in the
+  // resource paths. The |delegate| is responsible for handling user events.
   MenuState(const std::string& menu_name, Delegate* delegate);
 
   // GameState overrides
