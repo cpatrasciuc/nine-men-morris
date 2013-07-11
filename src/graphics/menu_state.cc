0370d2aaf51719d804a06d2a3aed123962f43a7f
Cristian Patrasciuc (Home) <cristian.patrasciuc@gmail.com>
2013-07-11 22:00:44 +0300

    Determine when a menu option was clicked and notify a Delegate.

--- src/graphics/menu_state.cc
+++ src/graphics/menu_state.cc
@@ -9,20 +9,47 @@
 #include "base/log.h"
 #include "graphics/ogre_app.h"
 
+#include "OGRE/OgreOverlay.h"
+#include "OGRE/OgreOverlayElement.h"
 #include "OGRE/OgreOverlayManager.h"
 
-namespace graphics {
+#include "OIS/OISMouse.h"
 
-MenuState::MenuState(const std::string& menu_name)
+namespace graphics {
+namespace {
+
+// TODO(menu_state): Remove this once the OGRE bug is fixed.
+// http://www.ogre3d.org/forums/viewtopic.php?f=2&t=63238
+void FixOgreBugWithOverlays(Ogre::Overlay* overlay) {
+  for (double x = 0.0; x <= 1.0; x += 0.05) {
+    for (double y = 0.0; y <= 1.0; y += 0.05) {
+      Ogre::OverlayElement* const element = overlay->findElementAt(x, y);
+      if (element) {
+        element->setCaption(element->getCaption());
+      }
+    }
+  }
+}
+
+}  // anonymous namespace
+
+MenuState::Delegate::Delegate() {}
+
+MenuState::Delegate::~Delegate() {}
+
+MenuState::MenuState(const std::string& menu_name, Delegate* delegate)
     : GameState(&OgreApp::Instance()),
       menu_name_(menu_name),
-      menu_overlay_(NULL) {}
+      delegate_(delegate),
+      menu_overlay_(NULL),
+      reload_captions_(false) {}
 
 bool MenuState::Initialize() {
   // TODO(menu_state): Check if the overlay should be destroyed.
   menu_overlay_ = Ogre::OverlayManager::getSingleton().getByName(menu_name_);
   DCHECK(menu_overlay_);
   menu_overlay_->show();
+  reload_captions_ = true;
   return true;
 }
 
@@ -39,6 +66,27 @@ void MenuState::Pause() {
 void MenuState::Resume() {
   DCHECK(menu_overlay_);
   menu_overlay_->show();
+}
+
+bool MenuState::mouseReleased(const OIS::MouseEvent& event,
+                              OIS::MouseButtonID id) {
+  const OIS::MouseState& state = event.state;
+  const double screen_x = state.X.abs / static_cast<double>(state.width);
+  const double screen_y = state.Y.abs / static_cast<double>(state.height);
+  const Ogre::OverlayElement* const element =
+      menu_overlay_->findElementAt(screen_x, screen_y);
+  if (element && !element->getCaption().empty()) {
+    delegate_->OnMenuOptionSelected(element->getCaption());
+  }
+  return true;
+}
+
+bool MenuState::frameRenderingQueued(const Ogre::FrameEvent& event) {
+  if (reload_captions_) {
+    FixOgreBugWithOverlays(menu_overlay_);
+    reload_captions_ = false;
+  }
+  return true;
 }
 
 }  // namespace graphics
--- src/graphics/menu_state.h
+++ src/graphics/menu_state.h
@@ -7,8 +7,13 @@
 
 #include <string>
 
+#include "base/basic_macros.h"
 #include "graphics/game_state.h"
 #include "graphics/graphics_export.h"
+
+#include "OGRE/OgreFrameListener.h"
+
+#include "OIS/OISMouse.h"
 
 namespace Ogre {
 class Overlay;
@@ -18,7 +23,20 @@ namespace graphics {
 
 class GRAPHICS_EXPORT MenuState : public GameState {
  public:
-  explicit MenuState(const std::string& menu_name);
+  class Delegate {
+   public:
+    virtual ~Delegate();
+
+    virtual void OnMenuOptionSelected(const std::string& option) = 0;
+
+   protected:
+    Delegate();
+
+   private:
+    DISALLOW_COPY_AND_ASSIGN(Delegate);
+  };
+
+  MenuState(const std::string& menu_name, Delegate* delegate);
 
   // GameState overrides
   virtual bool Initialize();
@@ -27,8 +45,16 @@ class GRAPHICS_EXPORT MenuState : public GameState {
   virtual void Resume();
 
  private:
+  // MouseListener overrides
+  virtual bool mouseReleased(const OIS::MouseEvent& event,
+                             OIS::MouseButtonID id);
+  // FrameListener overrides
+  virtual bool frameRenderingQueued(const Ogre::FrameEvent& event);
+
   std::string menu_name_;
+  Delegate* delegate_;
   Ogre::Overlay* menu_overlay_;
+  bool reload_captions_;
 };
 
 }  // namespace graphics
