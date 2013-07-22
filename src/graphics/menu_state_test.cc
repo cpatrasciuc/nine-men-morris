b26932eab4789202fb0340689935f5c95f9218f2
Cristian Patrasciuc (Home) <cristian.patrasciuc@gmail.com>
2013-07-22 22:16:53 +0300

    Add first phase on MenuStateTest.

---
+++ src/graphics/menu_state_test.cc
@@ -0,0 +1,113 @@
+// Copyright (c) 2013 Cristian Patrasciuc. All rights reserved.
+// Use of this source code is governed by a BSD-style license that can be
+// found in the LICENSE file.
+
+#include <memory>
+#include <string>
+
+#include "base/log.h"
+#include "graphics/in_game_test.h"
+#include "graphics/menu_state.h"
+#include "gtest/gtest.h"
+
+#include "OGRE/OgreOverlay.h"
+#include "OGRE/OgreOverlayContainer.h"
+#include "OGRE/OgreOverlayElement.h"
+#include "OGRE/OgreOverlayManager.h"
+
+#include "OIS/OISMouse.h"
+
+namespace graphics {
+namespace {
+
+const char kTestMenuOverlayName[] = "TestMenu";
+
+class MenuStateTest : public InGameTestBase {
+ public:
+  class TestMenuDelegate : public MenuState::Delegate {
+   public:
+    virtual void OnMenuOptionSelected(const std::string& option) {
+      LOG(INFO) << option;
+      option_ = option;
+    }
+
+    const std::string& selected_option() const { return option_; }
+    void clear_selection() { option_.clear(); }
+
+   private:
+    std::string option_;
+  };
+
+  virtual bool Initialize() {
+    delegate_ = new TestMenuDelegate;
+    menu_ = new MenuState(kTestMenuOverlayName,
+                          std::auto_ptr<MenuState::Delegate>(delegate_));
+    app()->PushState(menu_);
+    return true;
+  }
+
+  virtual void TestMethod() {
+    const char* const menu_buttons[] = {
+        "MenuButton1", "MenuButton2", "MenuButton3" };
+    const char* const menu_options[] = { "Option1", "Option2", "Option3" };
+
+    EXPECT_EQ(arraysize(menu_buttons), arraysize(menu_options));
+    for (size_t i = 0; i < arraysize(menu_buttons); ++i) {
+      delegate_->clear_selection();
+      SimulateClickOnOverlayElement(menu_buttons[i]);
+      EXPECT_EQ(menu_options[i], delegate_->selected_option()) << i;
+    }
+
+    delegate_->clear_selection();
+    SimulateClickAtScreenCoords(0, 0);
+    EXPECT_EQ("", delegate_->selected_option());
+
+    // TODO(menu_state_test): Add test for keyboard events.
+    SUCCEED();
+  }
+
+  virtual void Done() {
+    EXPECT_EQ(menu_, app()->PopState());
+    InGameTestBase::Done();
+  }
+
+ private:
+  void SimulateClickOnOverlayElement(const std::string& element_name) {
+    Ogre::Overlay* const menu_overlay =
+        Ogre::OverlayManager::getSingleton().getByName(kTestMenuOverlayName);
+    Ogre::OverlayContainer* const back_panel =
+        menu_overlay->getChild("BackPanel");
+    Ogre::OverlayElement* const button = back_panel->getChild(element_name);
+    const double x =
+        back_panel->getLeft() + button->getLeft() + button->getWidth() / 2.0;
+    const double y =
+        back_panel->getTop() + button->getTop() + button->getHeight() / 2.0;
+    EXPECT_TRUE(button->contains(x, y))
+        << button->getName() << ": " << x << " " << y;
+    EXPECT_TRUE(menu_overlay->findElementAt(x, y));
+    SimulateClickAtScreenCoords(x, y);
+  }
+
+  void SimulateClickAtScreenCoords(double screen_x, double screen_y) {
+    const OIS::MouseState& real_mouse = app()->mouse().getMouseState();
+    OIS::MouseState state(real_mouse);
+    state.X.abs = screen_x * state.width;
+    state.Y.abs = screen_y * state.height;
+    const OIS::MouseEvent move_event(NULL, state);
+    OIS::MouseListener* listener = static_cast<OIS::MouseListener*>(menu_);
+    listener->mouseMoved(move_event);
+    const OIS::MouseEvent press_event(NULL, state);
+    listener->mousePressed(press_event, OIS::MB_Left);
+    const OIS::MouseEvent release_event(NULL, state);
+    listener->mouseReleased(release_event, OIS::MB_Left);
+  }
+
+  // TODO(menu_state_test): Make this a scoped_ptr.
+  MenuState* menu_;
+  TestMenuDelegate* delegate_;
+};
+
+IN_GAME_TEST(MenuStateTest, MenuStateTest);
+
+}  // anonymous namespace
+}  // namespace graphics
