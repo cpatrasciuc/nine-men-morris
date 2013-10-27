d4ef2650bbad508057e9bcb54038c65d863960f6
Cristian Patrasciuc (Home) <cristian.patrasciuc@gmail.com>
2013-10-27 21:11:10 +0200

    Revert "Inline 'result' variable to avoid gcc warning about unused variable."
    This reverts commit 25bdb1aef36d94271d1565e3cc66b10f61512b1f.

--- src/base/log_unittest.cc
+++ src/base/log_unittest.cc
@@ -91,12 +91,14 @@ TEST_F(LogUnittest, LogMessageFormatTest) {
 }
 
 TEST_F(LogUnittest, DEBUG_ONLY_TEST(SystemErrorLogIfTest)) {
-  ELOG_IF(ERROR, std::pow(10.0f, 2.0f) == HUGE_VAL) << "Should not be logged";
+  float result = std::pow(10.0f, 2.0f);
+  ELOG_IF(ERROR, result == HUGE_VAL) << "Should not be logged";
   EXPECT_EQ(std::string(), test_stream().str());
 
   std::string message("Should be logged");
   float max_float = std::numeric_limits<float>::max();
-  ELOG_IF(ERROR, std::pow(max_float, max_float) == HUGE_VAL) << message;
+  result = std::pow(max_float, max_float);
+  ELOG_IF(ERROR, result == HUGE_VAL) << message;
   std::string output = test_stream().str();
   size_t pos = output.find(message);
   EXPECT_NE(std::string::npos, pos);
